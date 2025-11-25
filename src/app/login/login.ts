67fe8da6f6861ad81f38ba847d75b0456f9bf4ba
Mariia Katsala <ubuntovka@gmail.com>
2025-11-25 01:32:05 +0100

    login works

---
+++ src/app/login/login.ts
@@ -0,0 +1,47 @@
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router, RouterLink} from '@angular/router';
+import {LoginService} from '../services/login';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Login {
+  readonly password = new FormControl('');
+
+  loginForm = new FormGroup({
+    password: this.password,
+    // password: new FormControl()
+  });
+
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  constructor(protected loginService: LoginService, private router: Router) {
+  }
+
+
+  login() {
+    if (this.loginForm.valid) {
+      const ok = this.loginService.loginUser(this.password.value);
+      if (ok) {
+        // this.router.navigate(['home']); // or your desired route
+        console.log("yes");
+      } else {
+        console.log('Invalid password');
+      }
+    }
+  }
+}
---
+++ src/app/services/login.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+
+  loginUser(password: string | null): Boolean {
+    return password === "";
+  }
+
+}
